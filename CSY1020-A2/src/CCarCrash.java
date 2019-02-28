3953ffb75eff30137acf2156ba29f0cb42586cbc
cleanycloth <aidan.rayner@gmail.com>
2019-02-28 17:32:23 +0000

    Changed internal name (again). Added borders. Added icons to button grid. Added icons to actions. Some other stuff I forgot about.

--- CSY1020-A2/src/CCarCrash.java
+++ CSY1020-A2/src/CCarCrash.java
@@ -3,27 +3,28 @@ import java.awt.event.*;
 import javax.swing.*;
  
  
-public class CarApp extends JFrame 
+public class CCarCrash extends JFrame 
 {
 	//Added this next line to stop Java from pointlessly whinging.
 	private static final long serialVersionUID = 1L;
 	//Define new labels, text fields, buttons, and panels.
 	//d_ tagged objects are set to be disabled (i.e. non-clickable disabled buttons).
-	private JLabel OptionLabel, SquareLabel, DirectionLabel, TimerLabel, SliderLabel;
-    private JTextField OptionText, SquareText, DirectionText;
-    private JButton ExitButton, UpButton, DownButton, LeftButton, RightButton ,d_UpLeftButton, d_UpRightButton, d_CentreButton, d_DownLeftButton, d_DownRightButton, ActButton, RunButton, ResetButton, Op1Button, Op2Button, Op3Button, CompassButton;
-    private JPanel MainPanel, RightPanel, BottomPanel, DirectionPanel, TopPanel, ActionsPanel, SliderPanel, TimerPanel, OptionsPanel;
+	private JLabel OptionLabel, SquareLabel, DirectionLabel, TimerLabel, SliderLabel, TimerSepLabel, TimerSepLabel2;
+    private JTextField OptionText, SquareText, DirectionText, HourText, MinuteText, SecondText;
+	private JButton ExitButton, UpButton, DownButton, LeftButton, RightButton ,d_UpLeftButton, d_UpRightButton, d_CentreButton, d_DownLeftButton, d_DownRightButton, ActButton, RunButton, ResetButton, Op1Button, Op2Button, Op3Button, CompassButton;
+	private JButton GridButtons[] = new JButton[208];
+    private JPanel MainPanel, RightPanel, BottomPanel, DirectionPanel, TopPanel, ActionsPanel, SliderPanel, TimerLabelPanel, TimerPanel, OptionsPanel;
 	private JSlider SpeedSlider;
 	private static JMenuBar MenuBar;
 	private JMenu ScenarioMenu, EditMenu, ControlsMenu, HelpMenu;
-	private JMenuItem testitem;
-	private ImageIcon compassIcon;
+	private JMenuItem ExitMenuItem, HelpMenuItem, AboutMenuItem;
+	private ImageIcon CompassIcon, ActIcon, RunIcon, ResetIcon, BGEmptyIcon;
     //Create main frame
     public static void main (String[] args)
     {
 		MenuBar = new JMenuBar();
-        CarApp frame; 
-		frame = new CarApp();
+        CCarCrash frame; 
+		frame = new CCarCrash();
 		frame.setTitle("CCarCrash - Car Race Application");
         frame.setSize(810, 650);
 		frame.createGUI();
@@ -38,16 +39,20 @@ public class CarApp extends JFrame
     {
     	Container window = getContentPane();
     	MainPanel = new JPanel();
-        MainPanel.setPreferredSize(new Dimension(642, 538));
-        MainPanel.setBackground(Color.BLACK);
+		MainPanel.setPreferredSize(new Dimension(636, 542));
+        GridLayout MainLayout = new GridLayout(13,16);
+		MainPanel.setLayout(MainLayout);
+		MainPanel.setBorder(BorderFactory.createRaisedBevelBorder());
         window.add(MainPanel);
         
         RightPanel = new JPanel();
-        RightPanel.setPreferredSize(new Dimension(150, 532));
+		RightPanel.setPreferredSize(new Dimension(156, 542));
+		RightPanel.setBorder(BorderFactory.createRaisedBevelBorder());
         window.add(RightPanel);
         
         BottomPanel = new JPanel();
-        BottomPanel.setPreferredSize(new Dimension(790, 50));
+		BottomPanel.setPreferredSize(new Dimension(797, 50));
+		BottomPanel.setBorder(BorderFactory.createRaisedBevelBorder());
         window.add(BottomPanel);
         
         TopPanel = new JPanel();
@@ -56,8 +61,8 @@ public class CarApp extends JFrame
         DirectionPanel = new JPanel();
         DirectionPanel.setPreferredSize (new Dimension(150,120));
 
-		TimerPanel = new JPanel();
-		TimerPanel.setPreferredSize (new Dimension(150,120));
+		TimerLabelPanel = new JPanel();
+		TimerLabelPanel.setPreferredSize (new Dimension(150,120));
 		
 		OptionsPanel = new JPanel();
 		OptionsPanel.setPreferredSize (new Dimension(150,70));
@@ -65,11 +70,11 @@ public class CarApp extends JFrame
 		OptionsPanel.setLayout(OptionsLayout);
 		
 		ActionsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		ActionsPanel.setPreferredSize (new Dimension (480,40));
+		ActionsPanel.setPreferredSize (new Dimension (470,40));
 		BottomPanel.add(ActionsPanel);
 
 		SliderPanel = new JPanel();
-		SliderPanel.setPreferredSize (new Dimension(300,40));
+		SliderPanel.setPreferredSize (new Dimension(290,40));
 		BottomPanel.add(SliderPanel);
 		SliderLabel = new JLabel("Speed:");
 		SliderPanel.add(SliderLabel);
@@ -78,11 +83,19 @@ public class CarApp extends JFrame
 		
 		//Action buttons
 		ActButton = new JButton("Act");
-		
+		ActButton.setMargin(new Insets(0,10,0,10) );
 		RunButton = new JButton("Run");
+		RunButton.setMargin(new Insets(0,10,0,10) );
 		ResetButton = new JButton("Reset");
+		ResetButton.setMargin(new Insets(0,10,0,10) );
 		ActionsPanel.add(ActButton);
+		ActIcon = new ImageIcon("resources/step.png");
+		ActButton.setIcon(ActIcon);
 		ActionsPanel.add(RunButton);
+		RunIcon = new ImageIcon("resources/run.png");
+		RunButton.setIcon(RunIcon);
+		ResetIcon = new ImageIcon("resources/reset.png");
+		ResetButton.setIcon(ResetIcon);
 		ActionsPanel.add(ResetButton);
 
     }
@@ -118,13 +131,49 @@ public class CarApp extends JFrame
 	        DirectionText = new JTextField("Up");
 	        DirectionText.setHorizontalAlignment(JTextField.CENTER);
 	        TopPanel.add(DirectionLabel);
-	        TopPanel.add(DirectionText);
-
-			RightPanel.add(DirectionPanel);
+			TopPanel.add(DirectionText);
+			
+			//Place Direction buttons panel
 	        GridLayout directionLayout = new GridLayout(3,3);
 			DirectionPanel.setLayout(directionLayout);
+			RightPanel.add(DirectionPanel);
 			
-			RightPanel.add(TimerPanel);
+			//Place Timer Container
+			GridLayout timerLabelPanelLayout = new GridLayout(4,1);
+			TimerLabelPanel.setLayout(timerLabelPanelLayout);
+			RightPanel.add(TimerLabelPanel);
+			
+			//Create Timer Label
+			TimerLabel = new JLabel("Digital Timer");
+			TimerLabel.setHorizontalAlignment(JLabel.CENTER);
+			TimerLabelPanel.add(TimerLabel);
+
+			//Create actual Timer
+			TimerPanel = new JPanel();
+			TimerSepLabel = new JLabel(":");
+			TimerSepLabel2 = new JLabel(":");
+			TimerSepLabel.setHorizontalAlignment(JLabel.CENTER);
+			TimerSepLabel2.setHorizontalAlignment(JLabel.CENTER);
+			HourText = new JTextField("00");
+			MinuteText = new JTextField("00");
+			SecondText = new JTextField("00");
+			GridLayout timerLayout = new GridLayout(1,5);
+			TimerPanel.setLayout(timerLayout);
+			TimerLabelPanel.add(TimerPanel);
+			TimerPanel.add(HourText);
+			HourText.setEnabled(false);
+			HourText.setBackground(Color.BLACK);
+			HourText.setDisabledTextColor(Color.WHITE);
+			TimerPanel.add(TimerSepLabel);
+			TimerPanel.add(MinuteText);
+			MinuteText.setEnabled(false);
+			MinuteText.setBackground(Color.BLACK);
+			MinuteText.setDisabledTextColor(Color.WHITE);
+			TimerPanel.add(TimerSepLabel2);
+			TimerPanel.add(SecondText);
+			SecondText.setEnabled(false);
+			SecondText.setBackground(Color.BLACK);
+			SecondText.setDisabledTextColor(Color.WHITE);
 			RightPanel.add(OptionsPanel);
 	        
 	        //Create directional buttons
@@ -156,8 +205,7 @@ public class CarApp extends JFrame
 	        d_DownRightButton = new JButton("");
 			DirectionPanel.add(d_DownRightButton);    
 			
-			TimerLabel = new JLabel("Digital Timer");
-			TimerPanel.add(TimerLabel);
+			
 
 			//Create option buttons
 			Op1Button = new JButton("Option 1");
@@ -173,6 +221,7 @@ public class CarApp extends JFrame
 			Op3Button.setMargin(new Insets(1, 1, 1, 1) );
 			ExitButton.setMargin(new Insets(1, 1, 1, 1) );
 
+			//Create menu bar and menu items
 			ScenarioMenu = new JMenu("Scenario");
 			MenuBar.add(ScenarioMenu);
 			EditMenu = new JMenu("Edit");
@@ -182,12 +231,18 @@ public class CarApp extends JFrame
 			HelpMenu = new JMenu("Help");
 			MenuBar.add(HelpMenu);
 
-			testitem = new JMenuItem("test");
-			ScenarioMenu.add(testitem);
+			//Create selections for menu items
+			ExitMenuItem = new JMenuItem("Exit");
+			ScenarioMenu.add(ExitMenuItem);
+			HelpMenuItem = new JMenuItem("Help Topics");
+			HelpMenu.add(HelpMenuItem);
+			AboutMenuItem = new JMenuItem("About");
+			HelpMenu.add(AboutMenuItem);
 
-			compassIcon = new ImageIcon("resources/north.jpg");
+			//Create compass button (NOTE: This should change depending on direction, but does not do that yet!)
+			CompassIcon = new ImageIcon("resources/north.jpg");
 			CompassButton = new JButton();
-			CompassButton.setIcon(compassIcon);
+			CompassButton.setIcon(CompassIcon);
 			RightPanel.add(CompassButton);
 			
 			CompassButton.setMargin(new Insets(1, 1, 1, 1) );
@@ -198,6 +253,16 @@ public class CarApp extends JFrame
 	        d_DownLeftButton.setEnabled(false);
 	        d_DownRightButton.setEnabled(false);
 			d_CentreButton.setEnabled(false);
+
+			//Add buttons to main window
+			BGEmptyIcon = new ImageIcon("resources/space.png");
+			for (int nC=0; nC<208; nC++)
+			{
+				GridButtons[nC] = new JButton();
+				MainPanel.add(GridButtons[nC]);
+				GridButtons[nC].setIcon(BGEmptyIcon);
+			}
+
 		
 	    //Make the exit button exit the program 
         ExitButton.addActionListener(new ActionListener()
